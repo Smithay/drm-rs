5ce771b0e7cab07a81190ecc7b0149ba0219e7af
Tyler Slabinski <tslabinski@slabity.net>
2018-02-15 16:13:59 -0500

    Improve front-page documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,29 @@
+//! A safe interface to the Direct Rendering Manager subsystem found in various
+//! operating systems.
 //!
-//! # drm-rs
+//! # Summary
 //!
-//! drm-rs
+//! The Direct Rendering Manager (DRM) is subsystem found in various operating
+//! systems that exposes graphical functionality to userspace processes. It can
+//! be used to send data and commands to a GPU driver that implements the
+//! interface.
 //!
-//! This library is a safe interface to the Direct Rendering Manager API found on various operating systems.
+//! Userspace processes can access the DRM by opening a 'device node' (usually
+//! found in `/dev/dri/*`) and using various `ioctl` commands on the open file
+//! descriptor. Most processes use the libdrm library (part of the mesa project)
+//! to execute these commands. This crate takes a more direct approach,
+//! bypassing libdrm and executing the commands directly and doing minimal
+//! abstraction to keep the interface safe.
 //!
-//! This library is currently a work in progress.
+//! While the DRM subsystem exposes many powerful GPU interfaces, it is not
+//! recommended for rendering or GPGPU operations. There are many standards made
+//! for these use cases, and they are far more fitting for those sort of tasks.
 //!
 //! ## Usage
 //!
-//! The user is expected to implement their own functionality for opening and
-//! accessing the file descriptor of the device. Here we create a small wrapper
-//! around `File` and implement `AsRawFd`, `drm::Device`, and
-//! `drm::control::Device`:
-//!
-//! ```rust,ignore
-//! extern crate drm;
-//!
-//! use std::fs::{OpenOptions, File};
-//! use std::os::unix::io::{AsRawFd, RawFd};
-//!
-//! use drm::Device as BasicDevice;
-//! use drm::control::Device as ControlDevice;
-//!
-//! // The drm crate does not provide a method of opening the device.
-//! // It is expected to be implemented by the user.
-//! struct Card(File);
-//!
-//! // Required to implement drm::Device
-//! impl AsRawFd for Card {
-//!     fn as_raw_fd(&self) -> RawFd { self.0.as_raw_fd() }
-//! }
-//!
-//! // Required to implement drm::control::Device
-//! impl BasicDevice for Card { }
-//!
-//! // Allows modesetting functionality to be performed.
-//! impl ControlDevice for Card { }
-//!
-//! ```
-//!
-//! Assuming the program used the above wrapper, the user now opens the card:
-//!
-//! ```rust,ignore
-//! // Open the device (usually located at /dev/dri/*) with rw access.
-//! let mut options = OpenOptions::new();
-//! options.read(true);
-//! options.write(true);
-//! let file = options.open("/dev/dri/card0");
-//! let card = Card(file);
-//! ```
-//!
-//! Now we can check out what resources are available:
-//!
-//! ```rust,ignore
-//! // Get a set of all modesetting resource handles (excluding planes):
-//! let res_handles = card.resource_handles().unwrap();
-//!
-//! // Print all connector information
-//! for &con in res_handles.connectors() {
-//!     let info = card.resource_info(con).unwrap();
-//!
-//!     println!("{:#?}")
-//! }
-//!
-//! // Print all CRTC information
-//! for &crtc in res_handles.crtcs() {
-//!     let info = card.resource_info(crtc).unwrap();
-//!
-//!     println!("{:#?}")
-//! }
-//! ```
-//!
-//! You'll also want to find a suitable mode:
-//!
-//! ```rust,ignore
-//! // Assuming we found a good connector and loaded the info into `connector_info`
-//! let &mode = connector_info.modes().iter(); // Search until you find one you want.
-//! ```
-//!
-//! Once you find a suitable connector and CRTC, it's time to create a framebuffer.
-//! Here we use a simple dumbbuffer as the backend:'
-//!
-//! ```rust,ignore
-//! // Create a DB of size 1920x1080
-//! let db = dumbbuffer::DumbBuffer::create_from_device(&card, (1920, 1080), 32)
-//!     .expect("Could not create dumb buffer");
-//!
-//! // Map it and grey it out.
-//! let mut map = db.map(&card).expect("Could not map dumbbuffer");
-//! for mut b in map.as_mut() {
-//!     *b = 128; // Grey
-//! }
-//!
-//! let fb_info = framebuffer::Info::create_from_buffer(&card, &db)
-//! let fb_handle = fb_info.handle();
-//! ```
-//!
-//! Now we can apply the framebuffer onto the CRTC's internal plane, and connect it
-//! to a connector with the proper mode:
-//!
-//! ```rust,ignore
-//! // Assuming `crtc` is a crtc handle and `con` is a connector handle
-//! crtc.set_on_device(&card, fb_handle, &[con], (0, 0), Some(mode))
-//!     .expect("Could not set Crtc");
-//! ```
-//!
-//! The contents of the dumb buffer will now appear onto the screen.
+//! To begin using this crate, the [Device trait](Device.t.html) must be
+//! implemented. See the trait's [example](Device.t.html#example) section for
+//! details on how to implement it.
 
 #![warn(missing_docs)]
 
@@ -122,41 +39,63 @@ extern crate derive_more;
 extern crate error_chain;
 
 #[macro_use]
-extern crate failure;
-
-#[macro_use]
-pub mod ffi;
+pub(crate) mod ffi;
 pub mod result;
 
-pub mod control;
-pub mod buffer;
+use ffi::Wrapper;
 
 use std::os::unix::io::AsRawFd;
 use result::Result;
 
-use failure::Error;
-/// This trait can be implemented by any object that acts as a DRM device. It is
-/// a prerequisite for using any DRM functionality.
-
+/// This trait should be implemented by any object that acts as a DRM device. It
+/// is a prerequisite for using any DRM functionality.
+///
+/// This crate does not provide a concrete device object due to the various ways
+/// it can be implemented. The user of this crate is expected to implement it
+/// themselves and derive this trait as necessary. The example below demonstrates
+/// how to do this using a small wrapper.
+///
+/// # Example
+///
+/// ```
+/// extern crate drm;
+///
+/// use drm::Device;
+///
+/// use std::fs::File;
+/// use std::fs::OpenOptions;
+///
+/// use std::os::unix::io::RawFd;
+/// use std::os::unix::io::AsRawFd;
+///
+/// #[derive(Debug)]
+/// // A simple wrapper for a device node.
+/// struct Card(File);
+///
+/// // Implementing `AsRawFd` is a prerequisite to implementing the traits found
+/// // in this crate. Here, we are just calling `as_raw_fd()` on the inner File.
+/// impl AsRawFd for Card {
+///     fn as_raw_fd(&self) -> RawFd {
+///         self.0.as_raw_fd()
+///     }
+/// }
+///
+/// /// With `AsRawFd` implemented, we can now implement `drm::Device`.
+/// impl Device for Card {}
+///
+/// // Simple helper method for opening a `Card`.
+/// impl Card {
+///     fn open() -> Self {
+///         let mut options = OpenOptions::new();
+///         options.read(true);
+///         options.write(true);
+///
+///         // The normal location of the primary device node on Linux
+///         Card(options.open("/dev/dri/card0").unwrap())
+///     }
+/// }
+/// ```
 pub trait Device: AsRawFd {
-    /// Tells the DRM device whether we understand or do not understand a
-    /// particular capability.
-    ///
-    /// Some features, such as atomic modesetting, require informing the device
-    /// that the process can use such features before it will expose them.
-    fn set_client_cap(&self, cap: ClientCapability, set: bool) -> Result<()> {
-        let mut raw = {
-            let mut raw: ffi::drm_set_client_cap = Default::default();
-            raw.capability = cap as u64;
-            raw.value = set as u64;
-            raw
-        };
-
-        unsafe { ffi::ioctl_set_client_cap(self.as_raw_fd(), &mut raw)? };
-
-        Ok(())
-    }
-
     /// Acquires the DRM Master lock for this process.
     ///
     /// # Notes
@@ -167,47 +106,78 @@ pub trait Device: AsRawFd {
     ///
     /// This function is only available to processes with CAP_SYS_ADMIN
     /// privileges (usually as root)
-    fn set_master(&self) -> Result<()> {
-        unsafe { ffi::ioctl_set_master(self.as_raw_fd())? };
-
+    fn acquire_master_lock(&self) -> Result<()> {
+        unsafe { ffi::ioctl::acquire_master(self.as_raw_fd())?; }
         Ok(())
     }
 
     /// Releases the DRM Master lock for another process to use.
-    fn drop_master(&self) -> Result<()> {
-        unsafe { ffi::ioctl_drop_master(self.as_raw_fd())? };
-
+    fn release_master_lock(&self) -> Result<()> {
+        unsafe { ffi::ioctl::release_master(self.as_raw_fd())?; }
         Ok(())
     }
 
-    #[deprecated(note = "DRM Deprecated: Consider using a render node")]
+    #[deprecated(note="Consider opening a render node instead.")]
     /// Generates an [AuthToken](AuthToken.t.html) for this process.
     fn generate_auth_token(&self) -> Result<AuthToken> {
-        let token = {
-            let mut raw: ffi::drm_auth_t = Default::default();
-            unsafe { ffi::ioctl_get_magic(self.as_raw_fd(), &mut raw)? };
-            raw.magic
-        };
+        let mut t = ffi::GetToken::default();
+        t.ioctl(self.as_raw_fd())?;
 
-        Ok(AuthToken(token))
+        let token = AuthToken(t.raw_ref().magic);
+        Ok(token)
     }
 
-    #[deprecated(note = "DRM Deprecated: Consider using a render node")]
     /// Authenticates an [AuthToken](AuthToken.t.html) from another process.
+    ///
+    /// # Deprecation Notes
+    ///
+    /// A process should consider opening a render node instead of using
+    /// authentication tokens. However, this particular function is not marked
+    /// deprecated due to the need to authenticate older processes that do not
+    /// yet know about render nodes.
     fn authenticate_auth_token(&self, token: AuthToken) -> Result<()> {
-        let mut raw = ffi::drm_auth_t {
-            magic: token.into(),
-        };
-        unsafe { ffi::ioctl_auth_magic(self.as_raw_fd(), &mut raw)? };
+        let mut t = ffi::AuthToken::default();
+        t.raw_mut_ref().magic = token.into();
+        t.ioctl(self.as_raw_fd())?;
+        Ok(())
+    }
+
+    /// Requests the driver to expose or hide certain capabilities. See
+    /// [ClientCapability](ClientCapability.t.html) for more information.
+    fn toggle_capability(&self, cap: ClientCapability, enable: bool)
+                         -> Result<()> {
+        let mut t = ffi::SetCap::default();
+        t.raw_mut_ref().capability = cap as u64;
+        t.raw_mut_ref().value = enable as u64;
+        t.ioctl(self.as_raw_fd())?;
+
         Ok(())
     }
 }
 
-#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
-/// Capabilities that the process understands.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, From, Into)]
+/// An authentication token, unique to the file descriptor of the device.
 ///
-/// These can be used to tell the DRM device what capabilities the process can
-/// use.
+/// This token can be sent to another process that owns the DRM Master lock to
+/// allow unprivileged use of the device, such as rendering.
+///
+/// # Deprecation Notes
+///
+/// This method of authentication is somewhat deprecated. Accessing unprivileged
+/// functionality is best done by opening a render node. However, some other
+/// processes may still use this method of authentication. Therefore, we still
+/// provide functionality for generating and authenticating these tokens.
+pub struct AuthToken(u32);
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+/// Capabilities that can be toggled by the driver.
+///
+/// # Notes
+///
+/// Some DRM functionality is not immediately exposed by the driver. Before
+/// a process can access this functionality, we must ask the driver to
+/// expose it. This can be done using
+/// [toggle_capability](toggle_capability.t.html).
 pub enum ClientCapability {
     /// Stereoscopic 3D Support
     Stereo3D = ffi::DRM_CLIENT_CAP_STEREO_3D as isize,
@@ -216,21 +186,6 @@ pub enum ClientCapability {
     /// Atomic modesetting support
     Atomic = ffi::DRM_CLIENT_CAP_ATOMIC as isize,
 }
-
-#[deprecated(note = "DRM Deprecated: Consider opening a render node instead")]
-#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, From, Into)]
-/// An authentication token, unique to the process that determines who opened the
-/// device.
-///
-/// This token can be sent to another process that acts as the DRM Master to
-/// allow unprivileged use of the device.
-///
-/// # Deprecated
-///
-/// This method of authentication is deprecated. If you need to use the DRM
-/// device for unprivileged use, consider opening a Render Node, which requires
-/// no authentication to use.
-pub struct AuthToken(u32);
 
 #[allow(non_camel_case_types)]
 /// Signed point
