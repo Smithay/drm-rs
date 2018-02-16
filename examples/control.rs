bfc19aa2b80f303e8abe4fc44a7b20319d7b95f3
Tyler Slabinski <tslabinski@slabity.net>
2018-02-16 16:23:37 -0500

    Add control module examples

---
+++ examples/control.rs
@@ -0,0 +1,12 @@
+extern crate drm;
+
+/// Check the `util` module to see how the `Card` structure is implemented.
+pub mod util;
+use util::*;
+
+pub fn main() {
+    let card = Card::open_global();
+
+    let res = card.resource_handles();
+    println!("{:#?}", res);
+}
