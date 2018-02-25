7ce55f9780d0a392409ee599b9cba22ee4dd68e0
Tyler Slabinski <tslabinski@slabity.net>
2018-02-25 14:39:57 -0500

    Add simple control example

--- examples/control.rs
+++ examples/control.rs
@@ -4,12 +4,33 @@ extern crate drm;
 pub mod util;
 use util::*;
 
+use drm::control::connector;
+use drm::control::encoder;
+use drm::control::crtc;
+use drm::control::framebuffer;
+
 pub fn main() {
     let card = Card::open_global();
 
     let res = card.resource_handles().unwrap();
-    println!("{:?}", res.connectors());
-    println!("{:?}", res.encoders());
-    println!("{:?}", res.crtcs());
-    println!("{:?}", res.framebuffers());
+    println!("{:#?}", res);
+
+    for &i in res.connectors() {
+        println!("{:#?}", card.info::<connector::Info>(i).unwrap());
+    }
+
+    for &i in res.encoders() {
+        println!("{:#?}", card.info::<encoder::Info>(i).unwrap());
+    }
+
+    for &i in res.crtcs() {
+        println!("{:#?}", card.info::<crtc::Info>(i).unwrap());
+    }
+
+    for &i in res.framebuffers() {
+        println!("{:#?}", card.info::<framebuffer::Info>(i).unwrap());
+    }
+
+    let res = card.plane_handles().unwrap();
+    println!("{:#?}", res);
 }
