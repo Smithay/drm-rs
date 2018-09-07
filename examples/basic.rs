6a7627ea30d8e0f3ed8b2d0be323606d7a953c58
Tyler Slabinski <tslabinski@slabity.net>
2018-09-07 16:32:20 -0400

    Simplify basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,33 +7,27 @@ use util::*;
 pub fn main() {
     let card = Card::open_global();
 
-    println!("Generating AUTH token");
-    let token = card.generate_auth_token().unwrap();
-    println!("\t{:?}", token);
+    // Attempt to acquire and release master lock
+    println!("Get Master lock: {:?}", card.acquire_master_lock());
+    println!("Release Master lock: {:?}", card.release_master_lock());
 
-    println!("Aquiring Master Lock");
-    println!("\t{:?}", card.acquire_master_lock());
+    // Get the Bus ID of the device
+    println!("Getting Bus ID: {:?}", card.get_bus_id().unwrap().as_ref());
 
-    println!("Authenticating AUTH token");
-    println!("\t{:?}", card.authenticate_auth_token(token));
-
-    println!("Releasing Master Lock");
-    println!("\t{:?}", card.release_master_lock());
-
-    println!("Getting Bus ID");
-    println!("\t{:?}", card.get_bus_id().unwrap().as_ref());
-
+    // Figure out driver in use
     println!("Getting driver info");
     let driver = card.get_driver().unwrap();
     println!("\tName: {:?}", driver.name());
     println!("\tDate: {:?}", driver.date());
     println!("\tDesc: {:?}", driver.description());
 
+    // Enable all possible client capabilities
     println!("Setting client capabilities");
     for &cap in util::CLIENT_CAP_ENUMS {
         println!("\t{:?}: {:?}", cap, card.set_client_capability(cap, true));
     }
 
+    // Get driver capabilities
     println!("Getting driver capabilities");
     for &cap in util::DRIVER_CAP_ENUMS {
         println!("\t{:?}: {:?}", cap, card.get_driver_capability(cap));
